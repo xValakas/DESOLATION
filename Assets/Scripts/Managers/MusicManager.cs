e158eee1faac23b94ec80d4a794f1ab0ccd6583c
OSCAR SMITH <ES294335@falmouth.ac.uk>
2025-03-07 14:39:02 +0000

    Added volume slider functionality to MusicManager

--- Assets/Scripts/Managers/MusicManager.cs
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MusicManager : MonoBehaviour
 {
     [Header("Audio Source")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
+    [SerializeField] UnityEngine.Audio.AudioMixer mixer;
 
     [Header("Audio Clip")]
     public AudioClip background;
 
+    [Header("Settings Menu")]
+    public UnityEngine.UI.Slider volumeSlider;
+
     public static MusicManager instance;
+    public string MasterVolume;
     private void Awake()
     {
         if (instance == null)
@@ -24,6 +31,11 @@ public class MusicManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        float savedVol = PlayerPrefs.GetFloat(MasterVolume, volumeSlider.maxValue);
+        SetVolume(savedVol); 
+        volumeSlider.value = savedVol;
+        volumeSlider.onValueChanged.AddListener((float _) => SetVolume(_));
     }
 
     private void Start()
@@ -32,12 +44,23 @@ public class MusicManager : MonoBehaviour
         musicSource.Play();
     }
 
+    void SetVolume(float _value)
+    {
+        mixer.SetFloat(MasterVolume, ConvertToDecibel(_value / volumeSlider.maxValue));
+        PlayerPrefs.SetFloat(MasterVolume, _value);
+    }
 
 
-    public void PlaySFX(AudioClip clip)
+    public float ConvertToDecibel(float _value)
+    {
+        return Mathf.Log10(Mathf.Max(_value, 0.0001f)) * 20f;
+    }
+
+        public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
     }
+
 
 
 }
